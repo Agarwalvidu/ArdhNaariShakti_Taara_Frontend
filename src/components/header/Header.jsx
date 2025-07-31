51d3a6c3de3730b350488b8738f20872c9c2fe48
bhavy1204 <bhavysarwa51@gmail.com>
2025-07-31 11:05:24 +0530

    fix: nav-links in small screens

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,14 @@
-import React, { useContext } from "react"
 import logo from "../../assets/images/logo.png"
 import "./header.css"
 import { User } from "./User"
 import { nav } from "../../assets/data/data"
 import { Link } from "react-router-dom"
+import { useState } from "react"
 
 export const Header = () => {
-  
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   window.addEventListener("scroll", function () {
     const header = this.document.querySelector(".header")
     header.classList.toggle("active", this.window.scrollY > 100)
@@ -17,27 +19,52 @@ export const Header = () => {
       <header className='header'>
         <div className='scontainer flex'>
           <div className='logo'>
-          <Link to="/"> {/* Wrap the logo with Link */}
+            <Link to="/"> {/* Wrap the logo with Link */}
               <img src={logo} alt="logo" width="100px" />
             </Link>
           </div>
-          <nav>
+
+          <div className="mobileMenuBtn" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </div>
+
+          <nav className="navLinks">
             <ul>
               {nav.map((link) => (
                 <li key={link.id}>
-                {link.text === "taarabot" ? (
-                  <a href={link.url} style={{ color: 'white', fontWeight: 'bold',transition: 'font-size 0.3s' }} onMouseOver={e => e.target.style.color = 'black'} onMouseOut={e => e.target.style.color = 'white'} onMouseEnter={e => e.target.style.fontSize = '1.1em'}  // Increasing font size on mouse enter
-                  onMouseLeave={e => e.target.style.fontSize = '1em'} target="_blank" rel="noopener noreferrer">{link.text}</a>
-                ) : (
-                  <Link to={link.url} style={{ color: 'white', fontWeight: 'bold',transition: 'font-size 0.3s' }} onMouseOver={e => e.target.style.color = 'black'} onMouseOut={e => e.target.style.color = 'white'} onMouseEnter={e => e.target.style.fontSize = '1.1em'}  // Increasing font size on mouse enter
-                  onMouseLeave={e => e.target.style.fontSize = '1em'} >{link.text}</Link>
-                )}
-              </li>
+                  {link.text === "taarabot" ? (
+                    <a href={link.url} style={{ color: 'white', fontWeight: 'bold', transition: 'font-size 0.3s' }} onMouseOver={e => e.target.style.color = 'black'} onMouseOut={e => e.target.style.color = 'white'} onMouseEnter={e => e.target.style.fontSize = '1.1em'}  // Increasing font size on mouse enter
+                      onMouseLeave={e => e.target.style.fontSize = '1em'} target="_blank" rel="noopener noreferrer">{link.text}</a>
+                  ) : (
+                    <Link to={link.url} style={{ color: 'white', fontWeight: 'bold', transition: 'font-size 0.3s' }} onMouseOver={e => e.target.style.color = 'black'} onMouseOut={e => e.target.style.color = 'white'} onMouseEnter={e => e.target.style.fontSize = '1.1em'}  // Increasing font size on mouse enter
+                      onMouseLeave={e => e.target.style.fontSize = '1em'} >{link.text}</Link>
+                  )}
+                </li>
               ))}
             </ul>
           </nav>
-          <div className='account flexCenter'>
+          <div className='account desktopAccount'>
             <User />
+          </div>
+
+          {/* Hamburger meny logic */}
+          <div className={`mobileMenu ${isMenuOpen ? "open" : ""}`}>
+            <ul>
+              {nav.map((link) => (
+                <li key={link.id} onClick={()=>setIsMenuOpen(!isMenuOpen)}>
+                  {link.text === "taarabot" ? (
+                    <a href={link.url} target="_blank" rel="noopener noreferrer"> {link.text} </a>
+                  ) : (
+                    <Link to={link.url} > {link.text} </Link>
+                  )}
+                </li>
+              ))}
+            </ul>
+            <div className="account mobileAccount">
+              <User />
+            </div>
           </div>
         </div>
       </header>
